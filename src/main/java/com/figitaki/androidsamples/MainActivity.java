32069aa3f24c6c26e177360d819ba2ad9f1c27dc
Carey Janecka <figitaki96@yahoo.com>
2013-11-12 18:55:00 -0600

    Added a test case.

--- src/main/java/com/figitaki/androidsamples/MainActivity.java
+++ src/main/java/com/figitaki/androidsamples/MainActivity.java
@@ -3,6 +3,9 @@ package com.figitaki.androidsamples;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -16,6 +19,31 @@ public class MainActivity extends Activity {
         setContentView(R.layout.main);
     }
 
+    /** Creates the actions for the action bar */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /** Called when an options menu item is selected */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch(item.getItemId()) {
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
@@ -23,5 +51,15 @@ public class MainActivity extends Activity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    /** Called when the search bar is pressed */
+    private void openSearch() {
+        // TODO: Implement a search bar;
+    }
+
+    /** Called when the settings action is selected */
+    private void openSettings() {
+        // TODO: Create a settings activity
     }
 }
